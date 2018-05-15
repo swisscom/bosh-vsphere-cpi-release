55e77a305ba88fbb47aad9d77b035ef9b077fcbc
Mikhail Vyshegorodtsev <mvyshegorodtse@vmware.com>
2018-05-15 11:59:10 -0700

    Vendor gems before creating release

--- ci/tasks/cpi-networking-release-and-publish.sh
+++ ci/tasks/cpi-networking-release-and-publish.sh
@@ -4,6 +4,7 @@ mkdir  bosh-vsphere-cpi-networking-release
 
 pushd bosh-cpi-src
     ./compile-iso9660wrap.sh
+    ./src/vsphere_cpi/vendor_gems
     bosh create-release --tarball bosh-vsphere-cpi-networking-release.tgz --force
 popd
 
