c9eff6fffa1e81199f37124905d8c6c35f79980f
Mikhail Vyshegorodtsev <mvyshegorodtse@vmware.com>
2018-08-01 12:25:07 -0700

    Add teardown after test-upgrade

--- ci/tasks/teardown.sh
+++ ci/tasks/teardown.sh
@@ -2,17 +2,21 @@
 
 source source-ci/ci/shared/tasks/setup-env-proxy.sh
 
-if [ ! -e director-state/director-state.json ]; then
+if [ ! -e director-state-v65/director-state.json ]; then
   echo "director-state.json does not exist, skipping..."
   exit 0
 fi
 
-if [ -d "director-state/.bosh" ]; then
+if [ -d "director-state-v65/.bosh" ]; then
   # reuse compiled packages
   cp -r director-state/.bosh $HOME/
 fi
 
-pushd director-state > /dev/null
+cp director-config-v65/director.yml director-state-v65
+cp director-creds-v65/creds.yml director-state-v65
+cp director-env-v65/director.env director-state-v65
+
+pushd director-state-v65 > /dev/null
   # Don't exit on failure to delete existing deployments
   set +e
     source director.env
@@ -25,5 +29,5 @@ pushd director-state > /dev/null
   set -e
 
   echo "deleting existing BOSH Director VM..."
-  bosh -n delete-env --vars-store creds.yml -v director_name=bosh director.yml
+  bosh -n delete-env --state director-state.json --vars-store creds.yml -v director_name=bosh director.yml
 popd
